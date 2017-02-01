dec56757f0ee0e25fb17f7126e42c8e4f6fe2e93
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-01 18:08:22 +0000

    Wire up menu buttons

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNextScene ()
+    {
+        int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+        SceneManager.LoadScene (currentIndex + 1, LoadSceneMode.Single);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit ();
+    }
+
+}
