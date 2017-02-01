e69663435cb46afd681c83f46346ed88a6d37fcb
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-01 22:25:17 +0000

    Generate lanes and trees on grass lanes

---
+++ Assets/scripts/LaneSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class LaneSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] lanePrefabs;
+
+
+    private void Start ()
+    {
+        CreateRandomLane (0);
+        CreateRandomLane (10);
+        CreateRandomLane (20);
+        CreateRandomLane (30);
+        CreateRandomLane (40);
+    }
+
+    private void CreateRandomLane (float offset)
+    {
+        int laneIndex = Random.Range (0, lanePrefabs.Length);
+        GameObject lane = Instantiate (lanePrefabs [laneIndex]) as GameObject;
+        lane.transform.parent = transform;
+        lane.transform.position = new Vector3 (0, 0, offset);
+    }
+}
---
+++ Assets/scripts/TreeSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour {
+
+	[SerializeField]
+    private GameObject treePrefab;
+
+    private int maxTrees = 3;
+
+    private void Start ()
+    {
+        int numTrees = Random.Range (0, maxTrees);
+        for (int i = 0; i < maxTrees; i++)
+        {
+            CreateTree ();
+        }
+    }
+
+    private void CreateTree ()
+    {
+        GameObject tree = Instantiate (treePrefab) as GameObject;
+        tree.transform.parent = transform;
+        float randX = Random.Range (-40f, 40f);
+        float randZ = Random.Range (-5f, 5f);
+        tree.transform.localPosition = new Vector3 (randX, 0, randZ);
+    }
+
+}
