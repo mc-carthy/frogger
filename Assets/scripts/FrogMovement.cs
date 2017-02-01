d07211b5d183388a9bfe62d72943de8904e653ca
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-01 18:43:58 +0000

    Add basic level, along with basic frog jumping

---
+++ Assets/scripts/FrogMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FrogMovement : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 jumpForce;
+    private Rigidbody rb;
+
+    private void Awake ()
+    {
+        rb = GetComponent<Rigidbody> ();
+    }
+
+	private void Update ()
+    {
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            rb.AddForce (jumpForce, ForceMode.VelocityChange);
+        }
+    }
+
+}
