206e6b7e8a843a5e7091175993125549f95630cd
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-02 13:45:53 +0000

    Create moving vehicles and logs

---
+++ Assets/scripts/VehicleMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class VehicleMovement : MonoBehaviour {
+
+    float speed = 1000;
+
+    private void Update ()
+    {
+        transform.Translate (-speed * Time.deltaTime, 0, 0);
+    }
+
+}
