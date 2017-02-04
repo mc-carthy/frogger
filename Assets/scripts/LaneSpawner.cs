cc78b6cb1020288dbef2938168c30189f3d22002
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-04 16:01:40 +0000

    Ensure no consecutive lanes are dangerous

--- Assets/scripts/LaneSpawner.cs
+++ Assets/scripts/LaneSpawner.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 
+public enum LaneType { Safe, Danger }
+
 public class LaneSpawner : MonoBehaviour {
 
     [SerializeField]
-    private GameObject[] lanePrefabs;
+    private GameObject[] safeLanePrefabs;
+    [SerializeField]
+    private GameObject[] dangerLanePrefabs;
 
+    private LaneType lastLaneType = LaneType.Safe;
+    private float safeLaneProbability = 0.2f;
     private int numStartLanes = 20;
     private int laneWidth = 1000;
 
@@ -20,9 +26,33 @@ public class LaneSpawner : MonoBehaviour {
 
     private void CreateRandomLane (float offset)
     {
-        int laneIndex = Random.Range (0, lanePrefabs.Length);
-        GameObject lane = Instantiate (lanePrefabs [laneIndex]) as GameObject;
+        GameObject[] lanes;
+        if (lastLaneType == LaneType.Danger)
+        {
+            lanes = safeLanePrefabs;
+            lastLaneType = LaneType.Safe;
+        }
+        else
+        {
+            if (Random.value < safeLaneProbability)
+            {
+                lanes = safeLanePrefabs;
+                lastLaneType = LaneType.Safe;
+            }
+            else
+            {
+                lanes = dangerLanePrefabs;
+                lastLaneType = LaneType.Danger;
+            }
+        }
+        GameObject lane = InstantiateRandomLane (lanes);
         lane.transform.SetParent(transform, false);
         lane.transform.localPosition = new Vector3 (0, 0, offset);
+    }
+
+    private GameObject InstantiateRandomLane (GameObject[] lanes)
+    {
+        int laneIndex = Random.Range (0, lanes.Length);
+        return Instantiate (lanes [laneIndex]) as GameObject;
     }
 }
