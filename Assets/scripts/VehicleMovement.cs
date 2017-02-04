1e812d79d6a61f5489a199a915883347c1b2d5b1
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-04 15:31:48 +0000

    Cars drive on both sides of the road

--- Assets/scripts/VehicleMovement.cs
+++ Assets/scripts/VehicleMovement.cs
@@ -5,15 +5,18 @@ public class VehicleMovement : MonoBehaviour {
 
     private Rigidbody rb;
     private float speed = 1000;
+    private float xDir;
 
     private void Awake ()
     {
         rb = GetComponent<Rigidbody> ();
+        xDir = Mathf.Cos (Mathf.Deg2Rad * transform.parent.rotation.eulerAngles.y);
+        Debug.Log (xDir);
     }
 
     private void FixedUpdate ()
     {
-        rb.MovePosition (transform.position + new Vector3 (-speed * Time.fixedDeltaTime, 0, 0));
+        rb.MovePosition (transform.position + new Vector3 (-speed * xDir * Time.fixedDeltaTime, 0, 0));
     }
 
 }
